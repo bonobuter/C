68c882792d2495db69bae8999dc0e71267505a64
bonobuter <bonobuter03@gmail.com>
2022-12-07 20:43:20 +0100

    initial

---
+++ format.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// char %s plakt woord,
+// char %d plakt cijfer
+// char %f plakt cijfer met decimaal
+// [] maakt een string
+ int main()
+ {
+    char Test69 [] = "Ajax word kampioen";
+    int Test420 = 69420;
+  
+    printf("hand in hand want %s\n", Test69);
+    printf("kameraden %d\n", Test420);
+    printf("hand in hand want %s\n", Test69);
+    printf("voor feyenoord 1 \n");
+
+ return 0;
+}
---
+++ hello.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{ 
+    int getal_1;
+    int getal_2;
+
+    printf("vul je getal in: ");
+    scanf("%d", &getal_1);
+    printf("vul nog een getal in: ");
+    scanf("%d", &getal_2);
+    printf("de som is: %d", getal_1 + getal_2);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
